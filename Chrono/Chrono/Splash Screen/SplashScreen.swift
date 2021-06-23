1517f6d447df28bfc604dd41544b383ab7e7fe09
Sagar Patel <s.72427patel@gmail.com>
2021-06-22 18:10:45 -0700

    Change to 1 sec animation

--- Chrono/Chrono/Splash Screen/SplashScreen.swift
+++ Chrono/Chrono/Splash Screen/SplashScreen.swift
@@ -21,7 +21,7 @@ struct SplashScreen: View {
             }
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 withAnimation {
                     self.isActive = true
                 }
